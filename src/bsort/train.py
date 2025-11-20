029dae3ddf6b79217527cba439b618f084501eca
suirfan <suirfan20@gmail.com>
2025-11-20 11:15:26 +0700

    update

--- src/bsort/train.py
+++ src/bsort/train.py
@@ -3,9 +3,8 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Optional
 
-import yaml
-
 import wandb
+import yaml
 
 from .config import AppConfig
 from .model import BottleCapModel
